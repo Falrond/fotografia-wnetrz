1e6747fdb12bca6c7035c3eb9c51d0593b5bf5b9
Michał Winiecki <falrond@gmail.com>
2019-06-19 01:26:48 +0200

    edit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
-require("dotenv").config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
+// require("dotenv").config({
+//   path: `.env.${process.env.NODE_ENV}`,
+// })
 
 module.exports = {
   siteMetadata: {
@@ -19,9 +19,9 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        spaceId: "q250ppyz5yp7",
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken: "h2tQv-O-cEaZJB2kF6mp8ejxKDYZMm51IdQWiDWvb0A",
       },
     },
     `gatsby-plugin-transition-link`,
